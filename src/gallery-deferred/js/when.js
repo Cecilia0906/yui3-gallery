63913e48173ff3a945ed33adb6e3d39a5c8b4e02
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2011-08-13 16:21:31 -0300

    Used renamed _flatten()

--- src/gallery-deferred/js/when.js
+++ src/gallery-deferred/js/when.js
@@ -26,7 +26,7 @@ Y.defer = function (fn, context) {
  * @return Promise
  */
 Y.when = function () {
-	var deferreds = Y.Promise._spreadArray(YArray(arguments)),
+	var deferreds = Y.Promise._flatten(YArray(arguments)),
 		args = [],
 		resolved = 0,
 		rejected = 0;
