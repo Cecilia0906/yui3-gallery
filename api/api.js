f13660e5343db873e1bb7f0e787d9ff480273bfb
unknown <jdopazo@.admotion.local>
2012-04-03 12:45:22 -0300

    New documentation

---
+++ api/api.js
@@ -0,0 +1,20 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Plugin.NodeDeferred",
+        "Promise",
+        "YUI",
+        "io",
+        "io.Request"
+    ],
+    "modules": [
+        "gallery-deferred"
+    ],
+    "allModules": [
+        {
+            "displayName": "gallery-deferred",
+            "name": "gallery-deferred"
+        }
+    ]
+} };
+});
